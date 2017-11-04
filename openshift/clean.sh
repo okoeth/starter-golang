e74f2cb3877e827e0a226241ebf4c347ad3fa15f
Oliver Koeth <oliver.koeth@nttdata.com>
2017-11-04 08:42:40 +0100

    Test exit command

--- openshift/clean.sh
+++ openshift/clean.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 if [ x = x$1 -o x = x$2 ]; then
     echo "Usage: ./clean.sh <namespace> <service>"
-    exit
+    exit -1
 fi
 NAMESPACE=$1
 APP_NAME=$2
 
 oc delete all,pvc -l app=$APP_NAME -n $NAMESPACE
+if [ $? -ne 0 ]; then
+    echo "ERROR in ./clean.sh"
+    exit -1
+fi
+
+exit 0
