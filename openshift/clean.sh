bddb3b7af7a9211e974b2dab5e804bda725b8c2d
Oliver Koeth <oliver.koeth@nttdata.com>
2017-11-04 08:45:10 +0100

    Fix exit code

--- openshift/clean.sh
+++ openshift/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ x = x$1 -o x = x$2 ]; then
     echo "Usage: ./clean.sh <namespace> <service>"
-    exit -1
+    exit 1
 fi
 NAMESPACE=$1
 APP_NAME=$2
@@ -9,7 +9,7 @@ APP_NAME=$2
 oc deleter all,pvc -l app=$APP_NAME -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "ERROR in ./clean.sh"
-    exit -1
+    exit 1
 fi
 
 exit 0
