e16e78dbd031f859a255033b6e995377d86976d2
Oliver Koeth <oliver.koeth@nttdata.com>
2017-11-04 09:39:46 +0100

    Removing of old secrets

--- openshift/createBuildSecret.sh
+++ openshift/createBuildSecret.sh
@@ -7,13 +7,28 @@ NAMESPACE=$1
 USER=$2
 PASSWORD=$3
 
+SECRET_EXISTS=`oc get secrets -n $NAMESPACE | grep scmsecret | wc -l`
+if [ $SECRET_EXISTS -eq 1 ]; then
+    echo "Removing existing secret (ignoring errors)"
+
+    oc secrets link builder scmsecret -n $NAMESPACE
+    if [ $? -ne 0 ]; then
+        echo "WARNING: Ignoring error during unlinking of secret"
+    fi
+    
+    oc delete secret scmsecret -n $NAMESPACE
+    if [ $? -ne 0 ]; then
+        echo "WARNING: Ignoring error during deletion of secret"
+    fi
+fi
+
 oc secrets new-basicauth scmsecret --username=$USER --password=$PASSWORD -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "ERROR in ./createBuildSecret.sh"
     exit 1
 fi
 
-oc secrets add builder scmsecret -n $NAMESPACE
+oc secrets link builder scmsecret -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "ERROR in ./createBuildSecret.sh"
     exit 1
