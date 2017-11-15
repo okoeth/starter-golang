ab444a8080fe90ff47fcb9aaa59d2a628e86f8c4
Oliver Koeth <oliver.koeth@nttdata.com>
2017-11-15 20:34:49 +0100

    Fixed docker publish

--- integration/run_tests.sh
+++ integration/run_tests.sh
@@ -10,7 +10,6 @@ TEST_SERVER=$1
 
 echo "STEP 000: Wait for application to become ready"
 for i in 1 2 3 4 5 6 7 8 9 10; do
-    curl -k -X GET https://$TEST_SERVER/html/
     NOT_AVAIL=`curl -s -k -X GET \
     https://$TEST_SERVER/html/ | grep "Gopher" | wc -l`
     if [ $NOT_AVAIL -eq 0 ]; then
